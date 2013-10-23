54589b2df921f76bf0c76795920471a757446dad
Alex Kwan <alexkwan089@gmail.com>
2013-10-22 21:56:40 -0700

    adding in a second url

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/too', function(request, response){
+	response.send('Hello World TOO!');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
